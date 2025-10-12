1c9877b74ac358c3925ce606390667517199a567
Valeria <valelupopiatti@gmail.com>
2025-10-12 15:05:12 -0300

    cu registrar atencion

--- frontPeluqueria/src/components/Admin/atencion/modificarAtSer.tsx
+++ frontPeluqueria/src/components/Admin/atencion/modificarAtSer.tsx
@@ -50,7 +50,9 @@ const ModificarAtSer: React.FC = () => {
             if (filtroCategoria) query.append('idCategoria', filtroCategoria);
             
             try {
-                const url = `http://localhost:3000/api/producto/listarProductos?${query.toString()}`;
+                //const url = `http://localhost:3000/api/producto/listarProductos?${query.toString()}`;
+                const url = `http://localhost:3000/api/producto/}`;
+
 
                 const res = await fetch(url, {
                     headers: { Authorization: `Bearer ${token}` },
